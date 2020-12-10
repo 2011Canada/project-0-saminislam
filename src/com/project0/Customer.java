1b8bf17ffac26bafc7914ae3915fb3910d08de62
unknown <samin289@gmail.com>
2020-12-10 01:45:27 -0500

    Third Commit

--- src/com/project0/Customer.java
+++ src/com/project0/Customer.java
@@ -10,11 +10,9 @@ public class Customer  {
 	
 	protected String customer_name;
 	protected String password;
-	//hashmap might not be a good idea
-	//protected HashMap <Account, Double> List_of_accounts = new HashMap <Account, Double>();
-	protected List <Transaction> Transaction_list = new ArrayList<Transaction>();
-	protected List <Account> Account_list = new ArrayList<Account>();
-	protected static List <Customer> Customer_list = new ArrayList<Customer>();
+	protected List <Transaction> transaction_list = new ArrayList<Transaction>();
+	protected List <Account> account_list = new ArrayList<Account>();
+	protected static List <Customer> customer_list = new ArrayList<Customer>();
 	
 	public Customer() {
 	}
@@ -22,8 +20,8 @@ public class Customer  {
 	public Customer(String customer_name, String password, List<Account> List_of_accounts, List <Transaction> Transaction_list) {
 		this.customer_name = customer_name;
 		this.password = password;
-		this.Account_list = List_of_accounts;
-		this.Transaction_list = Transaction_list;
+		this.account_list = List_of_accounts;
+		this.transaction_list = Transaction_list;
 		
 		
 	}
@@ -36,13 +34,13 @@ public class Customer  {
 	public String toString() {
 		return ("Customer: " + this.customer_name +'\n'
 				+ "Password: " + this.password + '\n' + 
-				"List of Accounts: " + this.Account_list + '\n' +
-				"Transaction List: " + this.Transaction_list);
+				"List of Accounts: " + this.account_list + '\n' +
+				"Transaction List: " + this.transaction_list);
 	}
 
 	public static void verifyCustomer(String name, String password) {
 		
-		for (Customer item : Customer_list) {
+		for (Customer item : customer_list) {
 			if ((item.customer_name == name) && (item.password == password)) {
 				System.out.println("Account Verified!");
 				} else {
@@ -51,9 +49,59 @@ public class Customer  {
 			}
 		}
 	
+	// method to make sure that transaction object can actually be made
+	// to access this method, you have to do T.VerifyAccount(T,MS)
+	// so clean this code up where you only have one T
+	
+	// This works but come back and clean up
+	public boolean VerifyTransaction(Transaction T, Account money_spender) {
+		// first verification to see if person you are sending the money to is in DB
+		for ( Customer item : customer_list) {
+			if (item.customer_name == T.receiver) {
+				// second verification to check if person isn't sending negative money
+				if (T.amount_to_send > 0 ) {
+					// third verification to check if person has enough money to send 
+					if (money_spender.amount > T.amount_to_send) {
+						return true;
+					} else {
+						System.out.println("This account does not have enough money for a transaction");
+						return false;
+					}
+				} else {
+					System.out.println("Negative Number is rejected by the system");
+					return false;
+				}
+			}
+		}
+		
+		System.out.println("Sender was not found in our database. Please input the correct name");
+		return false;
+	}
+	
+	
+	
+	public void AcceptTransaction(Transaction T, Account money_getter, Account money_spender) {
+		if (T.amount_deposited == false) {
+			T.amount_deposited = true;
+			money_getter.amount = money_getter.amount + T.amount_to_send;
+			money_spender.amount = money_spender.amount - money_spender.amount;
+					
+		} else {
+			System.out.println("Transaction has already been accepted");
+		}
+			
+	}
+	
+	// THESE GOT PASTED FROM TRANSACTION CLASS
+	// CHECK TO SEE IF YOU NEED TO KEEP THE TRANSACTION METHODS
+	
 	public void withdraw(double w_amount, Account A ) {
 		A.amount = A.amount - w_amount;
 		
+	}
+	
+	public void deposit(double d_amount, Account A) {
+		A.amount = A.amount + d_amount;
 	}
 	
 	
--- src/com/project0/Mainprogram.java
+++ src/com/project0/Mainprogram.java
@@ -8,13 +8,38 @@ public class Mainprogram {
 
 	public static void main(String[] args) {
 		
+		
+		
+		Customer c1 = new Customer("SAMIN", "123x");
+		Customer.customer_list.add(c1);
+		Account a1 = new Account("Chequing", 2000);
+		c1.account_list.add(a1);
+		
+		Customer c2 = new Customer("RAKIN", "321a");
+		Customer.customer_list.add(c2);
+		Account a2 = new Account("Chequing",1500);
+		c2.account_list.add(a2);
+		
+		Transaction T = new Transaction("SAMIN", "RAKIN", 90000, "Chequing");
+		
+		boolean flag = T.VerifyTransaction(T,a1);
+		//System.out.println(flag);
+		
+		System.out.println(Customer.customer_list);
+		/*
+		
+		
 		List <Transaction> TL = new ArrayList<Transaction>();
 		Transaction T = new Transaction("SAMIN", "BABA", 1000.23, "Chequing");
 		TL.add(T);
 		//System.out.println(TL.toString());
 		//System.out.println("XXXXXX-----TRANSACTION STUFF ------XXXXXX" + '\n');
 				
-		Customer c1 = new Customer("BDS", "123x");
+
+		
+		
+		
+		Transaction T = new Transaction("SAMIN", "BABA", 1000.23, "Chequing");
 		//System.out.println(c1.toString());
 		c1.Transaction_list.add(T);
 		//System.out.println(c1);
@@ -28,6 +53,7 @@ public class Mainprogram {
 		//System.out.println("XXXXXX-----ACCOUNT STUFF ------XXXXXX" + '\n');
 		
 		Customer.Customer_list.add(c1);
+		
 		//System.out.println(Customer.Customer_list.get(0));
 		//System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 		//System.out.println((Customer.Customer_list.get(0)).password);
@@ -35,12 +61,16 @@ public class Mainprogram {
 		//System.out.println("XXXXXXXXXXXXX-----VERIFICATION ------XXXXXXXXXXXXXXXXXXXXXXXX");
 		//Customer.verifyCustomer("BDS","123x");
 		
-		// This proves that withdraw works!!!!
-		System.out.println(c1);
-		c1.withdraw(200.0,a1);
-		System.out.println(c1);
 		
 		
+		// This proves that withdraw and deposit works!!!!
+		System.out.println(c1);
+		c1.withdraw(200,a1);
+		System.out.println(c1);
+		c1.deposit(100, a1);
+		System.out.println(c1);
+		
+		*/
 		
 		
 	}
